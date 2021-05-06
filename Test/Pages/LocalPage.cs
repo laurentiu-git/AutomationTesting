bae204261ee1c0193ffa1911171e1f42610103ce
Laurentiu <Laurentiu@DESKTOP-DU0RLR6>
2021-05-06 15:05:40 +0300

    Checking Stuff

---
+++ Test/Pages/LocalPage.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Test.Base;
+using Test.Extensions;
+using Test.Utilities;
+
+namespace Test.Pages
+{
+    class LocalPage : Navigation
+    {
+
+        public void checkOurApplication()
+        {
+            var application = Browser.Driver.FindElement(Element.application, 20);;
+            Assert.That(application.Text.Equals("Our Application"));
+        }
+    }
+}
---
+++ Test/Steps/LocalSteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+using Test.Base;
+using Test.Pages;
+
+namespace Test.Steps
+{
+    [Binding]
+    class LocalSteps
+    {
+        private HomePage homePage;
+
+        [Given(@"I go to home page")]
+        public void GivenIGoToHomePage()
+        {
+            homePage = PageFactory.Instance.CurrentPage.GetInstance<HomePage>();
+            homePage.Navigate();
+        }
+
+        [Given(@"I click something")]
+        public void GivenIClickSomething()
+        {
+            var localPage = new LocalPage();
+            localPage.checkOurApplication();
+        }
+
+
+    }
+}
